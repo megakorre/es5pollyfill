65a0693ff6eab480a30902f1a6f45cc35684bc71
megakorre <patrik.karlin@gmail.com>
2011-07-05 04:16:56 -0700

    Edited polly.js via GitHub

---
+++ polly.js
@@ -0,0 +1,210 @@
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        res[i] = fun.call(thisp, t[i], i, t);
+    }
+
+    return res;
+  };
+}
+
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        fun.call(thisp, t[i], i, t);
+    }
+  };
+}
+
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+      {
+        var val = t[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, t))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
+}
+
+if (!Array.prototype.every)
+{
+  Array.prototype.every = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && !fun.call(thisp, t[i], i, t))
+        return false;
+    }
+
+    return true;
+  };
+}
+
+if (!Array.prototype.some)
+{
+  Array.prototype.some = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && fun.call(thisp, t[i], i, t))
+        return true;
+    }
+
+    return false;
+  };
+}
+
+if ( !Array.prototype.reduce ) {
+  Array.prototype.reduce = function reduce(accumlator){
+        var i, l = this.length, curr;
+        
+        if(typeof accumlator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+          throw new TypeError("First argument is not callable");
+
+        if((l == 0 || l === null) && (arguments.length <= 1))// == on purpose to test 0 and false.
+          throw new TypeError("Array length is 0 and no second argument");
+        
+        if(arguments.length <= 1){
+          for(i=0 ; i=l) // empty array
+            throw new TypeError("Empty array and no second argument");
+            
+          curr = this[i++]; // Increase i to start searching the secondly defined element in the array
+        }
+        else{
+          curr = arguments[1];
+        }
+        
+        for(i = i || 0 ; i < l ; i++){
+          if(i in this)
+            curr = accumlator.call(undefined, curr, this[i], i, this);
+        }
+        
+        return curr;
+     };
+}
+
+if (!Array.prototype.reduceRight)
+{
+  Array.prototype.reduceRight = function(callbackfn /*, initialValue */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof callbackfn !== "function")
+      throw new TypeError();
+
+    // no value to return if no initial value, empty array
+    if (len === 0 && arguments.length === 1)
+      throw new TypeError();
+
+    var k = len - 1;
+    var accumulator;
+    if (arguments.length >= 2)
+    {
+      accumulator = arguments[1];
+    }
+    else
+    {
+      do
+      {
+        if (k in this)
+        {
+          accumulator = this[k--];
+          break;
+        }
+
+        // if array contains no values, no initial value to return
+        if (--k < 0)
+          throw new TypeError();
+      }
+      while (true);
+    }
+
+    while (k >= 0)
+    {
+      if (k in t)
+        accumulator = callbackfn.call(undefined, accumulator, t[k], k, t);
+      k--;
+    }
+
+    return accumulator;
+  };
+}
+
